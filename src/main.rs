f9a61801adde1891c25aee5edb423bc4b3ea695f
Khalid Ali <khalludi123@gmail.com>
2024-07-04 15:00:23 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+use std::collections::HashMap;
+use std::fmt::Display;
 use axum::{
     routing::{get, post},
     http::StatusCode,
     Json, Router,
 };
-use serde::{Deserialize, Serialize};
+use serde::{de, Deserialize, Deserializer, Serialize};
 use std::process::Command;
 use std::fs::File;
 use std::io;
 use std::io::Write;
+use std::str::FromStr;
+use serde::de::DeserializeOwned;
 
 #[tokio::main]
 async fn main() {
@@ -30,6 +34,8 @@ async fn root() -> &'static str {
 }
 
 async fn run_code(Json(payload): Json<CreateCode>) -> (StatusCode, Json<Output>) {
+    println!("{:?}", payload);
+
     let input_filename = "test.py";
     let mut file = File::create(input_filename).expect("unable to create file");
     file.write_all(payload.code.as_bytes()).expect("could not write code to file");
@@ -71,9 +77,36 @@ async fn run_code(Json(payload): Json<CreateCode>) -> (StatusCode, Json<Output>)
 #[derive(Deserialize, Debug)]
 struct CreateCode {
     code: String,
+    #[serde(rename = "testCaseHash")]
+    test_cases: HashMap<String, TestCase>
+}
+
+#[derive(Deserialize, Debug)]
+struct TestCase {
+    #[serde(rename = "numCourses")]
+    num_courses: String,
+    //#[serde(deserialize_with = "json_from_str")]
+    prerequisites: String,
 }
 
 #[derive(Serialize, Debug)]
 struct Output {
     output: String,
+}
+
+fn primitive_from_str<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+    where T: FromStr,
+          T::Err: Display,
+          D: Deserializer<'de>
+{
+    let s = String::deserialize(deserializer)?;
+    T::from_str(&s).map_err(de::Error::custom)
+}
+
+fn json_from_str<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+    where T: DeserializeOwned,
+          D: Deserializer<'de>
+{
+    let s = String::deserialize(deserializer)?;
+    serde_json::from_str(&s).map_err(de::Error::custom)
 }
